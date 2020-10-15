589bcf4f01bb2750bc70e839663929c8fd2de412
Cristian Montenegro <crisdarmon@gmail.com>
2020-10-15 18:12:34 -0500

    -added the zombie and simple animations

--- Assets/Scripts/EnemyFollower.cs
+++ Assets/Scripts/EnemyFollower.cs
@@ -1,51 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyFollower : MonoBehaviour
 {
-	private bool isAttacking = false;
 	[SerializeField] private float speed = 3f;
 	[SerializeField] private int attackDamage = 1;
 	private Transform playerPosition;
-	private Rigidbody enemyRigidbody;
+	private NavMeshAgent navMesh = default;
+	private bool isFollowingPlayer = false;
+	private bool isAttacking = false;
 
-    void Start()
-    {
+	public bool IsFollowingPlayer => isFollowingPlayer;
+	public bool IsAttacking => isAttacking;
+
+	void Start()
+	{
 		playerPosition = GameObject.Find("Player").GetComponent<Transform>();
-		enemyRigidbody = GetComponent<Rigidbody>();
-    }
+		navMesh = GetComponent<NavMeshAgent>();
+	}
 
     void FixedUpdate()
 	{
 		MoveToThePlayer();
 		AttackThePlayer();
 		LookThePlayer();
+		StopInPlayerPosition();
+		Debug.Log(isAttacking);
 	}
 
 	private void LookThePlayer()
 	{
-		transform.LookAt(playerPosition.position);
+       // transform.LookAt(playerPosition.position);
 	}
 
 	private void MoveToThePlayer()
 	{
-		Vector3 positionToMove = Vector3.MoveTowards(transform.position, playerPosition.position, speed * Time.deltaTime);
-		enemyRigidbody.MovePosition(positionToMove);
+		Vector3 positionToMove = Vector3.MoveTowards(transform.position, playerPosition.position, navMesh.acceleration * Time.deltaTime);
+		navMesh.destination = positionToMove;
+		isFollowingPlayer = true;
 	}
 
 	private void AttackThePlayer()
 	{
-		if (Vector2.Distance(transform.position, playerPosition.position) < 0.2f && !isAttacking)
+		if (Vector2.Distance(transform.position, playerPosition.position) < 1f && !isAttacking)
 		{
 			isAttacking = true;
-			speed = 0;
-			StartCoroutine(secDelay());
-			StartCoroutine(attackDelay());
+			navMesh.acceleration = 0;
+			StartCoroutine(AttackDelay());
 		}
+		else
+			navMesh.acceleration = navMesh.acceleration;
 	}
 
-	private IEnumerator attackDelay()
+	private void StopInPlayerPosition()
+    {
+		if (Vector3.Distance(transform.position, playerPosition.position) < 1.5f)
+        {
+			speed = 0f;
+        }
+    }
+
+	private IEnumerator AttackDelay()
 	{
 		if (isAttacking)
 		{
@@ -55,10 +72,5 @@ public class EnemyFollower : MonoBehaviour
 			yield return new WaitForSeconds(2);
 			isAttacking = false;
 		}
-	}
-
-	private IEnumerator secDelay()
-	{
-		yield return new WaitForSeconds(1);
 	}
 }
---
+++ Assets/Scripts/EnemyFollowerAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowerAnimations : MonoBehaviour
+{
+    private Animator animator;
+    private EnemyFollower enemy;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        enemy = GetComponent<EnemyFollower>();
+    }
+
+    void Update()
+    {
+        if (enemy.IsFollowingPlayer)
+            animator.SetBool("isFollow", true);
+        else
+            animator.SetBool("isFollow", false);
+
+        if (enemy.IsAttacking)
+            animator.SetBool("isAttacking", true);
+        else 
+            animator.SetBool("isAttacking", false);
+    }
+}
