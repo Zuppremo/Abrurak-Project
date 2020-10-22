17d082001349c5daa665199ccb36a59ec96697eb
DarkDemonSupreme <santimountblack@gmail.com>
2020-10-21 23:24:07 -0500

    -Removed Unused Assets -Main Menu, loading scene

---
+++ Assets/Scripts/GunSounds.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GunSounds : MonoBehaviour
+{
+	[SerializeField] private AudioClip gunFiredClip = default;
+	[SerializeField] private AudioClip gunReloadRequired = default;
+	[SerializeField] private AudioClip gunReloadStarted = default;
+
+	private Gun gun;
+	private AudioSource audioSource;
+
+	
+    private void Awake()
+    {
+		gun = GetComponent<Gun>();
+		audioSource = gameObject.AddComponent<AudioSource>();
+		gun.Fired += OnGunFired;
+		gun.ReloadRequired += OnReloadRequired;
+		gun.ReloadStarted += OnReloadStarted;
+    }
+
+	private void OnGunFired(bool hasAppliedDamage)
+	{
+		audioSource.PlayOneShot(gunFiredClip);
+	}
+
+	private void OnReloadRequired()
+	{
+		audioSource.PlayOneShot(gunReloadRequired);
+	}
+
+	private void OnReloadStarted(float reloadTime)
+	{
+		audioSource.PlayOneShot(gunReloadStarted);
+	}
+
+	private void OnDestroy()
+	{
+		gun.Fired -= OnGunFired;
+		gun.ReloadRequired -= OnReloadRequired;
+		gun.ReloadStarted -= OnReloadStarted;
+	}
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public Animator fade;
+
+	public void SwapLevel()
+	{
+		StartCoroutine(ChangeLevel());
+	}
+
+	public IEnumerator ChangeLevel()
+	{
+		fade.Play("FadeOut");
+		yield return new WaitForSeconds(3);
+		SceneManager.LoadScene(1);
+	}
+}
